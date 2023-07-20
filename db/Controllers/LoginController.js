f10d6f412646aa3cba59942c09bb4972299f6fcb
saud khan <saudkhanbpk@gmail.com>
2023-07-20 00:11:07 -0700

    login done

---
+++ db/Controllers/LoginController.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../Models/User');
+
+const LoginController = {
+  async signin(req, res) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ Error: true, msg: "Please enter email and password" });
+    }
+
+    try {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(404).json({ Error: true, msg: "User not found" });
+      }
+
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (!isPasswordMatch) {
+        return res.status(401).json({ Error: true, msg: "Invalid credentials" });
+      }
+
+      if (!user.verify) {
+        return res.status(401).json({ Error: true, msg: "User not verified. Please verify your email first." });
+      }
+
+      const token = jwt.sign(
+        { email: user.email, id: user._id },
+        "your_secret_key"
+      );
+
+      return res.status(200).json({
+        user,
+        token,
+        msg: "User signed in successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({ Error: true, msg: "Internal Server Error" });
+    }
+  },
+};
+
+module.exports = LoginController;
--- db/routes/Router.js
+++ db/routes/Router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../Controllers/controller');
-const general_Information = require('../Controllers/generalController')
+const general_Information = require('../Controllers/generalController');
+const LoginController = require('../Controllers/LoginController');
 router.post('/register', controller.register);
+router.post('/login', LoginController.signin)
 router.post('/user_verify', controller.verificationAfterEmail);
 router.post('/general_info', general_Information.general);
 
