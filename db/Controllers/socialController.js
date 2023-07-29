02374e69fd322bd22fad6b278b8b731da33800c6
saud khan <saudkhanbpk@gmail.com>
2023-07-28 22:01:43 -0700

    error remove

--- db/Controllers/socialController.js
+++ db/Controllers/socialController.js
@@ -3,47 +3,37 @@ const jwt = require('jsonwebtoken');
 const Socialcontroller = {
     async social(req, res) {
         const { userId, instagram, tiktok, youtube, facebook, twitter, pinterest, linkedin, blog } = req.body;
-        console.log(instagram, tiktok, youtube, facebook, twitter, pinterest, linkedin, blog)
+        console.log(instagram, tiktok, youtube, facebook, twitter, pinterest, linkedin, blog);
+
         if (!instagram || !tiktok || !youtube || !facebook || !twitter || !pinterest || !linkedin || !blog) {
             return res.status(400).json({ Error: true, msg: "Please enter all fields" });
         }
-
         try {
             const user = await Social.find({ instagram, tiktok, youtube, facebook, twitter, pinterest, linkedin, blog });
-            if (user.length === 0) {
+            const newUser = new Social({
+                instagram,
+                tiktok,
+                youtube,
+                facebook,
+                twitter,
+                pinterest,
+                linkedin,
+                blog,
+                userId
+            });
 
-                const newUser = new Social({
-                    instagram,
-                    tiktok,
-                    youtube,
-                    facebook,
-                    twitter,
-                    pinterest,
-                    linkedin,
-                    blog,
-                    userId
+            await newUser.save().then((result) => {
+                console.log('newUser', newUser);
+                const token = jwt.sign(
+                    { instagram: newUser.instagram, id: newUser._id },
+                    "your_secretc_key"
+                );
+                return res.status(201).json({
+                    newUser,
+                    token,
+                    msg: "Social handle Created Successfully",
                 });
-
-                await newUser.save().then((result) => {
-                    console.log('newUser', newUser)
-                    const token = jwt.sign(
-                        { instagram: newUser.instagram, id: newUser._id },
-                        "your_secretc_key"
-                    );
-
-                    return res.status(201).json({
-                        newUser,
-                        token,
-                        msg: "Social handle Created Successfully",
-                    });
-                });
-            } else {
-                return res.status(400).json({
-                    status: false,
-                    Error: true,
-                    msg: "Social handle already exists",
-                });
-            }
+            });
         } catch (error) {
             return res.status(500).json({ Error: true, msg: "Internal Server Error" });
         }
